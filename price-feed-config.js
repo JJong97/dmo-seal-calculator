463f91375312388006d1cb026c564f019c2a8c2f
JJong97 <88355545+JJong97@users.noreply.github.com>
2026-09-07 00:55:39 +0900

    Add files via upload

---
+++ price-feed-config.js
@@ -0,0 +1,3 @@
+// Google Sheets에서 "웹에 게시 → CSV"로 만든 주소를 따옴표 안에 넣습니다.
+window.DMO_PRICE_FEED_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSXnzliFkXzGbFzeXGwbfQZ0hya3C7PdvscUE9GqCdphVMifuhVUQ940NY8iHCGew/pub?gid=404666208&single=true&output=csv";
+window.DMO_PRICE_GVIZ_URL = "https://docs.google.com/spreadsheets/d/1iosA-WkXQvK_sjqRJatrpZmEHwELf-4H/edit?gid=404666208";
